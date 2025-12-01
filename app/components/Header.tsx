726ba7ab05e5e2ef42e7e83692c30ebcd524e0ce
Haya <hayachannel5@gmail.com>
2025-12-01 22:13:02 +0900

    fix: ヘッダーがウィンドウの横幅によってズレるのを修正

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,7 @@
 export default function Header() {
   return (
-    <header className="bg-gray-800 text-white">
-      <div className="container mx-auto px-4 py-1">
-        <h1 className="text-lg text-gray-200 text-left">
-          Discord 日程調整アンケート作成ツール｜AppLii
-        </h1>
-      </div>
+    <header className="bg-gray-800 text-white shadow-md">
+      <h1 className="text-gray-200 text-left py-1 px-6">Discord 日程調整アンケート作成ツール｜AppLii</h1>
     </header>
   );
 }
